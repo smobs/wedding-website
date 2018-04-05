63534250eeb04da8afdd1b2761e8c56ee91d762c
Toby Smyth <tobs169@gmail.com>
2018-04-05 07:06:45 +0100

    Crappy form

--- src/Handler/Invite.hs
+++ src/Handler/Invite.hs
@@ -1,24 +1,40 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Handler.Invite where
 
 import Import
 
 getInviteR :: Handler Html
-getInviteR = do 
-    defaultLayout
-        $(widgetFile "invite")
+getInviteR = do
+  defaultLayout $(widgetFile "invite")
+
 getInfoR :: Handler Html
-getInfoR = do 
-    defaultLayout
-        $(widgetFile "info")
+getInfoR = do
+  defaultLayout $(widgetFile "info")
 
 getRsvpR :: Handler Html
-getRsvpR = do 
-    defaultLayout
-        $(widgetFile "invite")
+getRsvpR = do
+    (formWidget, formEnctype) <- generateFormPost guestRsvpForm
+    defaultLayout $(widgetFile "rsvp")
 
 postRsvpR :: Handler Html
-postRsvpR =  do 
-    defaultLayout
-        $(widgetFile "invite")
+postRsvpR = do
+  defaultLayout $(widgetFile "invite")
 
+guestRsvpForm :: Form GuestRsvp
+guestRsvpForm =
+  renderDivs $
+  GuestRsvp <$> (areq yesNo textSettings (Just False)) <*>
+  (areq textField textSettings Nothing) <*>
+  (areq yesNo textSettings (Just False))
+  where
+    textSettings =
+      FieldSettings
+      { fsLabel = "Dietary requirements"
+      , fsTooltip = Nothing
+      , fsId = Nothing
+      , fsName = Nothing
+      , fsAttrs = [("class", "form-control"), ("placeholder", "I hate peas")]
+      }
+    yesNo = radioFieldList [("Yes" :: Text, True), ("No" :: Text, False)]
