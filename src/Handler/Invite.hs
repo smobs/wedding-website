21c70931bacf17acdf911ef35aff2349cbe9bf98
Toby Smyth <tobs169@gmail.com>
2018-04-08 15:52:37 +0100

    Lookout old values

--- src/Handler/Invite.hs
+++ src/Handler/Invite.hs
@@ -16,23 +16,35 @@ getInfoR = do
 getRsvpR :: Handler Html
 getRsvpR = do
   (egid, _) <- requireAuthPair
-  case egid of 
-    Left gid -> do 
-      (formWidget, formEnctype) <- generateFormPost $ guestRsvpForm gid
+  case egid of
+    Left gid -> do
+      mguest <- runDB (getBy (UniqueRsvp gid))
+      let guest = case mguest of
+            Just (Entity _ g)  -> g
+            _ -> GuestRsvp gid True Nothing False 
+      (formWidget, formEnctype) <- generateFormPost $ guestRsvpForm guest
       defaultLayout $(widgetFile "rsvp")
     _ -> defaultLayout $(widgetFile "invite")
+
 postRsvpR :: Handler Html
 postRsvpR = do
   defaultLayout $(widgetFile "invite")
 
 yesNo = radioFieldList [("Yes" :: Text, True), ("No" :: Text, False)]
 
-guestRsvpForm :: GuestId -> Form GuestRsvp
-guestRsvpForm gid = do
-  let rsvp = GuestRsvp gid <$> areq yesNo ("Can you come" {fsAttrs = [("class", "attending-control")]}) (Just True) 
-                   <*> areq textField textSettings Nothing 
-                   <*> areq yesNo ("Do you need the bus" {fsAttrs = [("class", "bus-control")]}) (Just True) 
-  
+guestRsvpForm :: GuestRsvp -> Form GuestRsvp
+guestRsvpForm (GuestRsvp gid coming diet bus) = do
+  let rsvp =
+        GuestRsvp (gid) <$>
+        areq
+          yesNo
+          ("Can you come" {fsAttrs = [("class", "attending-control")]})
+          (Just coming) <*>
+        aopt textField textSettings (Just diet) <*>
+        areq
+          yesNo
+          ("Do you need the bus" {fsAttrs = [("class", "bus-control")]})
+          (Just bus)
   renderDivs rsvp
   where
     textSettings =
@@ -43,4 +55,3 @@ guestRsvpForm gid = do
       , fsName = Nothing
       , fsAttrs = [("class", "dietcontrol"), ("placeholder", "I hate peas")]
       }
-
