dc2ea2420bf773c22a22498949b6eef155ec5db6
Toby Smyth <tobs169@gmail.com>
2018-04-12 18:57:20 +0100

    Rsvp by party

--- src/Handler/Invite.hs
+++ src/Handler/Invite.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
 module Handler.Invite where
 
 import Import
@@ -15,57 +16,55 @@ getInfoR = do
 
 getRsvpR :: Handler Html
 getRsvpR = do
-  mguest <- getUserRsvp
+  mguest <- getGuestId
   case mguest of
     Just guest -> do
-      (formWidget, formEnctype) <- generateFormPost $ guestRsvpForm guest
+      party <- wholePartyRsvp guest
+      (formWidget, formEnctype) <- generateFormPost $ guestRsvpForm party
       defaultLayout $(widgetFile "rsvp")
     Nothing -> defaultLayout $(widgetFile "invite")
 
 postRsvpR :: Handler Html
 postRsvpR = do
-  mguest <- getUserRsvp
+  mguest <- getGuestId
   case mguest of
     Just guest -> do
-      ((res, widget), enctype) <- runFormPost $ guestRsvpForm guest
+      party <- wholePartyRsvp guest
+      ((res, widget), enctype) <- runFormPost $ guestRsvpForm party
       case res of
-        FormSuccess g@(GuestRsvp gid _ _ _) -> do
+        FormSuccess gs -> do
           runDB $ do 
-            deleteBy (UniqueRsvp gid)
-            insert_ g 
+            traverse_ updateRsvp gs
         _ -> pure ()
       defaultLayout $(widgetFile "invite")
     _ -> defaultLayout $(widgetFile "invite")
+  
+updateRsvp :: GuestRsvp -> DB ()
+updateRsvp g@(GuestRsvp gid _ _ _) = do 
+  deleteBy (UniqueRsvp gid)
+  insert_ g
 
-getUserRsvp :: Handler (Maybe GuestRsvp)
-getUserRsvp = do
+getGuestId :: Handler (Maybe GuestId)
+getGuestId = do
   (egid, _) <- requireAuthPair
   case egid of
-    Left gid -> do
-      mguest <- runDB (getBy (UniqueRsvp gid))
-      let guest =
-            case mguest of
-              Just (Entity _ g) -> g
-              _ -> GuestRsvp gid True Nothing False
-      return (Just guest)
+    Left gid -> pure $ Just gid
     _ -> return Nothing
 
+defaultRsvp gid = GuestRsvp gid True Nothing False
 yesNo = radioFieldList [("Yes" :: Text, True), ("No" :: Text, False)]
 
-guestRsvpForm :: GuestRsvp -> Form GuestRsvp
-guestRsvpForm (GuestRsvp gid coming diet bus) = do
-  let rsvp =
-        GuestRsvp (gid) <$>
-        areq
-          yesNo
-          ("Can you come" {fsAttrs = [("class", "attending-control")]})
-          (Just coming) <*>
-        aopt textField textSettings (Just diet) <*>
-        areq
-          yesNo
-          ("Do you need the bus" {fsAttrs = [("class", "bus-control")]})
-          (Just bus)
-  renderDivs rsvp
+rsvpAForm :: GuestRsvp -> AForm (HandlerT App IO) GuestRsvp
+rsvpAForm (GuestRsvp gid coming diet bus) = GuestRsvp (gid) <$>
+            areq
+              yesNo
+              ("Can you come" {fsAttrs = [("class", "attending-control")]})
+              (Just coming) <*>
+            aopt textField textSettings (Just diet) <*>
+            areq
+              yesNo
+              ("Do you need the bus" {fsAttrs = [("class", "bus-control")]})
+              (Just bus)
   where
     textSettings =
       FieldSettings
@@ -75,3 +74,17 @@ guestRsvpForm (GuestRsvp gid coming diet bus) = do
       , fsName = Nothing
       , fsAttrs = [("class", "dietcontrol"), ("placeholder", "I hate peas")]
       }
+
+
+guestRsvpForm :: [GuestRsvp] -> Form [GuestRsvp]
+guestRsvpForm gs = do
+  renderDivs (traverse rsvpAForm gs)
+
+wholePartyRsvp :: GuestId -> Handler [GuestRsvp]
+wholePartyRsvp gid = runDB $ do
+  gs <- wholeParty' gid
+  rsvps <-  traverse (getBy . UniqueRsvp) gs
+  pure $ (maybe (defaultRsvp gid) (\(Entity _ rsvp) -> rsvp)) <$> rsvps
+
+wholeParty' :: GuestId -> DB [GuestId]
+wholeParty' g = pure [g]
