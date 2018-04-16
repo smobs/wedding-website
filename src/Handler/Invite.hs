582a30c44fc4d615a9029d2d9788db9a939c10af
Toby Smyth <tobs169@gmail.com>
2018-04-16 18:32:26 +0100

    Add error and fix subtle bug

--- src/Handler/Invite.hs
+++ src/Handler/Invite.hs
@@ -3,16 +3,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE RankNTypes #-}
+
 module Handler.Invite where
 
-import Import
 import Data.Guest
+import Import
 import Yesod
 
-type NamedRsvp = (GuestRsvp)
+type NamedRsvp = (Guest, GuestRsvp)
 
-data PartyRsvp = Solo NamedRsvp | Couple NamedRsvp NamedRsvp
-
+data PartyRsvp
+  = Solo NamedRsvp
+  | Couple NamedRsvp
+           NamedRsvp
+  deriving (Show)
 
 getInviteR :: Handler Html
 getInviteR = do
@@ -34,109 +38,119 @@ getAccommodationR :: Handler Html
 getAccommodationR = do
   defaultLayout $(widgetFile "accommodation")
 
-
 getRsvpR :: Handler Html
 getRsvpR = do
-  guest <- getGuestId
+  guest <- getGuest
   party <- wholePartyRsvp guest
-  (formWidget, formEnctype) <- generateFormPost $ rsvpMForm party
+  ((res, formWidget), formEnctype) <- runFormPost $ rsvpMForm party
   defaultLayout $(widgetFile "rsvp")
 
 postRsvpR :: Handler Html
 postRsvpR = do
-  guest <- getGuestId
+  guest <- getGuest
   party <- wholePartyRsvp guest
-  handleRsvpPost party   
-  defaultLayout $(widgetFile "invite")
+  liftIO $ print party
+  handleRsvpPost party
 
-handleRsvpPost :: PartyRsvp -> Handler ()
+handleRsvpPost :: PartyRsvp -> Handler Html
 handleRsvpPost rsvp = do
   ((res, widget), enctype) <- runFormPost $ rsvpMForm rsvp
+  liftIO $ print res
   case res of
     FormSuccess gs -> do
       runDB $ do
-        case gs of 
-          Solo me -> updateRsvp me
-          Couple one two -> do 
-            updateRsvp one
-            updateRsvp two
-    _ -> do
-      liftIO $ print "fail"
-      pure ()
-  
+        case gs of
+          Solo me -> updateRsvp (snd me)
+          Couple one two -> do
+            updateRsvp (snd one)
+            updateRsvp (snd two)
+      defaultLayout $(widgetFile "invite")
+    FormFailure pageErrors -> do
+      defaultLayout $(widgetFile "errors")
+
 updateRsvp :: GuestRsvp -> DB ()
-updateRsvp g@(GuestRsvp gid _ _ _) = do 
+updateRsvp g@(GuestRsvp gid _ _ _) = do
   deleteBy (UniqueRsvp gid)
   insert_ g
 
-getGuestId :: Handler (GuestId)
-getGuestId = do
-  (egid, _) <- requireAuthPair
-  pure egid
+getGuest :: Handler (Entity Guest)
+getGuest = requireAuth
 
 defaultRsvp gid = GuestRsvp gid True Nothing False
+
 yesNo = radioFieldList [("Yes" :: Text, True), ("No" :: Text, False)]
 
-
 rsvpMForm :: PartyRsvp -> Html -> MForm Handler (FormResult PartyRsvp, Widget)
-rsvpMForm (Solo g) e = do 
+rsvpMForm (Solo g) e = do
   (r, w) <- rsvpMForm' g e
   pure (Solo <$> r, w)
-rsvpMForm (Couple o t) e = do 
+rsvpMForm (Couple o t) e = do
   (r1, w1) <- rsvpMForm' o e
-  (r2, w2) <- rsvpMForm' t e
-  let w = [whamlet|
+  (r2, w2) <- rsvpMForm' t mempty
+  let w =
+        [whamlet|
       ^{w1}
       ^{w2}
     |]
-  pure (Couple <$> r1 <*> r2 , w)
+  pure (Couple <$> r1 <*> r2, w)
 
 rsvpMForm' :: NamedRsvp -> Html -> MForm Handler (FormResult NamedRsvp, Widget)
-rsvpMForm' (GuestRsvp gid coming diet bus)  extra = do
-  let comingW = mreq
-              yesNo
-              ("Can you come" {fsAttrs = [("class", "attending-control")]})
-              (Just coming)
-  let dietW = mopt textField ("Can you come" {fsAttrs = [("class", "dietcontrol"), ("placeholder", "I hate peas")]}) (Just diet)
-  let busW = mreq
-              yesNo
-              ("Do you need a transfer between the reception and the venue" {fsAttrs = [("class", "bus-control")]})
-              (Just bus)
+rsvpMForm' (guest, GuestRsvp gid coming diet bus) extra = do
+  let comingW =
+        mreq
+          yesNo
+          ("Can you come" {fsAttrs = [("class", "attending-control")]})
+          (Just coming)
+  let dietW =
+        mopt
+          textField
+          ("Can you come"
+           { fsAttrs =
+               [("class", "dietcontrol"), ("placeholder", "I hate peas")]
+           })
+          (Just diet)
+  let busW =
+        mreq
+          yesNo
+          ("Do you need a transfer between the reception and the venue"
+           {fsAttrs = [("class", "bus-control")]})
+          (Just bus)
   (comingRes, comingView) <- comingW
-
-  (dietRes, dietView) <- dietW 
+  (dietRes, dietView) <- dietW
   (busRes, busView) <- busW
-  let widget = [whamlet|
+  let widget =
+        [whamlet|
       #{extra}
       ^{fvInput comingView}
       ^{fvInput busView}
       ^{fvInput dietView}
     |]
-  let guestRes = GuestRsvp gid <$> comingRes <*> dietRes <*> busRes 
+  let guestRes =
+        (,) guest <$> (GuestRsvp gid <$> comingRes <*> dietRes <*> busRes)
   pure (guestRes, widget)
 
-wholePartyRsvp :: GuestId -> Handler PartyRsvp
-wholePartyRsvp gid = runDB $ do
-  rsvp <- lookupRsvp gid
-  mplusone <- plusone gid
-  case mplusone of
-    Just poid -> do
-      prsvp <- lookupRsvp poid 
-      pure (Couple rsvp prsvp)
-    _ -> pure (Solo rsvp)
+wholePartyRsvp :: Entity Guest -> Handler PartyRsvp
+wholePartyRsvp g =
+  runDB $ do
+    rsvp <- lookupRsvp g
+    mplusone <- plusone g
+    case mplusone of
+      Just poid -> do
+        prsvp <- lookupRsvp poid
+        pure (Couple rsvp prsvp)
+      _ -> pure (Solo rsvp)
 
-lookupRsvp :: GuestId -> DB (GuestRsvp)
-lookupRsvp i = maybe (defaultRsvp i) (\(Entity _ rsvp) -> rsvp) <$> (getBy $ UniqueRsvp i)
+lookupRsvp :: Entity Guest -> DB (NamedRsvp)
+lookupRsvp (Entity i g) = do
+  rsvp <-
+    maybe (defaultRsvp i) (\(Entity _ rsvp) -> rsvp) <$> (getBy $ UniqueRsvp i)
+  pure (g, rsvp)
 
-plusone :: GuestId -> DB (Maybe GuestId)
-plusone g = do
-    mpid <- get g  
-    case mpid of 
-      Just (Guest _ _ _ partyId) -> do 
-          eguests <- lookupParty partyId
-          let others = filter (\k -> k /= g) $ (\(Entity k _) -> k) <$> eguests
-          pure (listToMaybe others)
-      _ -> pure Nothing
+plusone :: Entity Guest -> DB (Maybe (Entity Guest))
+plusone (Entity g (Guest _ _ _ partyId)) = do
+  eguests <- lookupParty partyId
+  let others = filter (\(Entity k _) -> k /= g) $ eguests
+  pure (listToMaybe others)
 
 lookupParty :: Party -> DB [Entity Guest]
 lookupParty partyId = selectList [GuestParty ==. partyId] [LimitTo 2]
