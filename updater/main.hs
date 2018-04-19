3984cbc81addd362fd2727ca127b1dc81673765b
Toby Smyth <tobs169@gmail.com>
2018-04-19 18:49:53 +0100

    Methods for writing out the rsvps as csv

--- updater/main.hs
+++ updater/main.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 
 import Control.Monad.Logger (runStderrLoggingT)
+import Control.Monad.Logger (NoLoggingT)
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import Data.Guest
 import Database.Persist
 import Database.Persist.Postgresql
-import Import
+import Import hiding ((.=))
 import Options.Applicative as O
 
 data Command
@@ -19,18 +21,26 @@ data Inputs = Inputs
   , databaseConnection :: ConnectionString
   }
 
+newtype NamedRsvp =
+  MkNamedRsvp (Text, Maybe GuestRsvp)
+
+instance ToNamedRecord NamedRsvp where
+  toNamedRecord (MkNamedRsvp (n, Nothing)) =
+    namedRecord ["Name" .= n, "Coming" .= na, "Diet" .= na, "Bus" .= na]
+    where
+      na :: Text
+      na = "No reply"
+
+instance DefaultOrdered NamedRsvp where
+  headerOrder _ = ["Name", "Coming", "Diet", "Bus"]
+
 main :: IO ()
 main = do
   options <- execParser opts
   let conn = databaseConnection options
   case inputCommand options of
-    Download fileName -> pure ()
-    Upload fileName -> do
-      csvcontents <- BL.readFile fileName
-      let egs = decodeGuests csvcontents
-      case egs of
-        Left er -> putStrLn (pack er)
-        Right gs -> updateGuests conn gs
+    Download file -> downloadRsvp conn file
+    Upload file -> updateGuestList conn file
 
 opts :: O.ParserInfo Inputs
 opts =
@@ -67,20 +77,42 @@ decodeGuests' ::
   -> Either String (Vector (GuestUserName, FirstName, LastName, Party))
 decodeGuests' b = decode HasHeader b
 
+encodeRsvp :: [NamedRsvp] -> BL.ByteString
+encodeRsvp rs = encodeDefaultOrderedByName rs
+
 connStr =
   "host=localhost dbname=postgres user=postgres password=mysecretpassword port=4321"
 
 type HandleDB m a = ReaderT SqlBackend m a
 
-updateGuests :: ConnectionString -> Vector Guest -> IO ()
-updateGuests conn gs = do
+updateGuestList :: ConnectionString -> String -> IO ()
+updateGuestList conn file = do
+  csvcontents <- BL.readFile file
+  let egs = decodeGuests csvcontents
+  case egs of
+    Left er -> putStrLn (pack er)
+    Right gs -> updateGuests conn gs
+
+downloadRsvp :: ConnectionString -> String -> IO ()
+downloadRsvp conn file = do
+  rsvps <- getRsvps conn
+  let csv = encodeRsvp rsvps
+  BL.writeFile file csv
+
+runDbCommand ::
+     ConnectionString -> HandleDB (NoLoggingT (ResourceT IO)) a -> IO a
+runDbCommand conn cmd = do
   runStderrLoggingT $
     withPostgresqlPool conn 10 $ \pool ->
       liftIO $ do
         flip runSqlPersistMPool pool $ do
           runMigration migrateAll
-          _ <- traverse upsertGuest gs
-          pure ()
+          cmd
+
+updateGuests :: ConnectionString -> Vector Guest -> IO ()
+updateGuests conn gs = do
+  _ <- runDbCommand conn $ traverse upsertGuest gs
+  pure ()
 
 upsertGuest :: MonadIO m => Guest -> HandleDB m ()
 upsertGuest g@(Guest usrname _ _ _) = do
@@ -88,3 +120,9 @@ upsertGuest g@(Guest usrname _ _ _) = do
   case mg of
     Nothing -> insert_ g
     Just (Entity gid _) -> replace gid g
+
+getRsvps :: ConnectionString -> IO [NamedRsvp]
+getRsvps conn = runDbCommand conn readRsvp
+
+readRsvp :: MonadIO m => HandleDB m [NamedRsvp]
+readRsvp = pure []
